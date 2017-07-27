cabc5080bc25c4903728d50758fecec76156d946
Corn Julio <leith.hawkins@lls.nsw.gov.au>
2017-07-27 13:32:49 +1000

    Script includes For Loop based  on Dissolved in Memory output

--- Wild_Pig_Update.py
+++ Wild_Pig_Update.py
@@ -16,40 +16,38 @@ try:
     spRef = 'N:\\Projection_FIles\\WGS_84.prj'
 
     # Make the XY event layer...
-    arcpy.MakeXYEventLayer_management(in_Table, x_coords, y_coords, out_Layer, spRef, z_coords)
+    arcpy.MakeXYEventLayer_management(in_Table, x_coords, y_coords, out_Layer, spRef)
 
     # Print the total rows
-    print(arcpy.GetCount_management(out_Layer))
+    #print(arcpy.GetCount_management(out_Layer))
 
     # Save to a layer file
     arcpy.SaveToLayerFile_management(out_Layer, saved_Layer)
-    out_FGDB = 'N:\\Wild_Pig_Project\\Wild_Pig_output.gdb\\Glen_Innes'
-    out_FC = '\\' + 'Glen_Innes6'
+    out_FGDB = 'N:\\Wild_Pig_Project\\Wild_Pig_output.gdb\\Glen_Innes\\'
+    out_FC = '\\' + 'Glen_Innes'
+    #arcpy.CopyFeatures_management(saved_Layer, out_FGDB + out_FC)
+    #print('Copy Complete')
+    arcpy.env.outputMFlag = "Disabled"
+    arcpy.env.outputZFlag = "Disabled"
     arcpy.FeatureClassToFeatureClass_conversion(saved_Layer, out_FGDB, out_FC)
 
 except Exception as err:
     print(err.args[0])
 
-
-    # with arcpy.da.SearchCursor('N:\\Wild_Pig_Project\\Wild_Pig_output.gdb\\Glen_Innes\\Glen_Innes6', 'Device_ID') as cursor:
-    #     for row in cursor:
-    #         dev_id = str(row[0])
-    #     print (dev_id)
-
 def unique_values(table, field):
     with arcpy.da.SearchCursor(table, [field]) as cursor:
         return sorted({row[0] for row in cursor})
-        print (row[0])
 
-List_ofValues = unique_values('N:\\Wild_Pig_Project\\Wild_Pig_output.gdb\\Glen_Innes\\Glen_Innes6', 'Device_ID')
+Glen_Innes ='N:\\Wild_Pig_Project\\Wild_Pig_output.gdb\\Glen_Innes\\Glen_Innes'
+Glen_Innes_Dissolve ='N:\\Wild_Pig_Project\\Wild_Pig_output.gdb\\Glen_Innes\\Glen_Innes_Dissolcve'
+List_ofValues = unique_values(Glen_Innes, 'Device_ID')
 print (List_ofValues)
+arcpy.MakeFeatureLayer_management(Glen_Innes, 'tempLayer')
+arcpy.Dissolve_management('tempLayer', 'in_memory/device', 'Device_ID')
+print ('Dissolve Complete')
 
-for Device_ID in unique_values:
-    dev_id = str(Device_ID)
-    print (dev_id)
+with arcpy.da.SearchCursor('in_memory/device' ,['Device_ID']) as dissolve_feature:
+    for row in dissolve_feature:
+        refNumber = str(row[0])
+        print(refNumber)
 
-
-# with arcpy.da.SearchCursor(List_ofValues) as cursor:
-#     for row in cursor:
-#         dev_id = str(row[0])
-#     print (dev_id)
