3b93815f25ff4233594249c5c3925d26f11241ca
Corn Julio <leith.hawkins@lls.nsw.gov.au>
2017-07-27 14:43:51 +1000

    changed Script to run to all collars FGDB instead of just GI

--- Wild_Pig_Update.py
+++ Wild_Pig_Update.py
@@ -6,7 +6,7 @@ arcpy.env.workspace = "C:\\Users\\hawkinle\\Desktop\\STDTAS"
 arcpy.env.overwriteOutput = True
 try:
     # Set the local variables
-    in_Table = "GPS_072417_201818.csv"
+    in_Table = "GPS_072717_002024.CSV"
     x_coords = "Longitude"
     y_coords = "Latitude"
     z_coords = "Altitude"
@@ -23,8 +23,8 @@ try:
 
     # Save to a layer file
     arcpy.SaveToLayerFile_management(out_Layer, saved_Layer)
-    out_FGDB = 'N:\\Wild_Pig_Project\\Wild_Pig_output.gdb\\Glen_Innes'
-    out_FC = '\\' + 'Glen_Innes'
+    out_FGDB = 'N:\Wild_Pig_Project\All_Collars.gdb\All_Collars'
+    out_FC = '\\' + 'All_Collars'
     #arcpy.CopyFeatures_management(saved_Layer, out_FGDB + out_FC)
     #print('Copy Complete')
     arcpy.env.outputMFlag = "Disabled"
@@ -34,15 +34,14 @@ try:
 except Exception as err:
     print(err.args[0])
 
-def unique_values(table, field):
-    with arcpy.da.SearchCursor(table, [field]) as cursor:
-        return sorted({row[0] for row in cursor})
+# def unique_values(table, field):
+#     with arcpy.da.SearchCursor(table, [field]) as cursor:
+#         return sorted({row[0] for row in cursor})
 
-Glen_Innes ='N:\\Wild_Pig_Project\\Wild_Pig_output.gdb\\Glen_Innes\\Glen_Innes'
-Glen_Innes_Dissolve ='N:\\Wild_Pig_Project\\Wild_Pig_output.gdb\\Glen_Innes\\Glen_Innes_Dissolcve'
-List_ofValues = unique_values(Glen_Innes, 'Device_ID')
-print (List_ofValues)
-arcpy.MakeFeatureLayer_management(Glen_Innes, 'tempLayer')
+All_Collars = out_FGDB + out_FC
+# List_ofValues = unique_values(All_Collars, 'Device_ID')
+# print (List_ofValues)
+arcpy.MakeFeatureLayer_management(All_Collars, 'tempLayer')
 arcpy.Dissolve_management('tempLayer', 'in_memory/device', 'Device_ID')
 print ('Dissolve Complete')
 
@@ -57,3 +56,6 @@ with arcpy.da.SearchCursor('in_memory/device' ,['Device_ID']) as dissolve_featur
         arcpy.SelectLayerByAttribute_management('templayer', 'NEW_SELECTION', Selection_q)
         arcpy.FeatureClassToFeatureClass_conversion('templayer', feature, 'Device_ID' + refNumber)
 
+arcpy.Delete_management('templayer')
+arcpy.Delete_management('in_memory/device')
+del row
