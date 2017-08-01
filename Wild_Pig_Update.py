35f9782b9a1f82a95b5cc660fc59a52d46964cbf
Corn Julio <leith.hawkins@lls.nsw.gov.au>
2017-08-01 13:04:45 +1000

    Added Git ignore files and updated Main Code

--- Wild_Pig_Update.py
+++ Wild_Pig_Update.py
@@ -22,7 +22,8 @@ try:
 
     # Save to a layer file create feature class - update datefield and delete
     arcpy.SaveToLayerFile_management(out_Layer, saved_Layer)
-    out_FGDB = 'N:\Wild_Pig_Project\All_Collars.gdb\All_Collars'
+    out_FGDB = 'N:\\Wild_Pig_Project\\All_Collars.gdb\\All_Collars'
+    out_Heat = 'N:\\Wild_Pig_Project\\All_Collars.gdb\\Heatmap_Collars'
     out_FC = '\\' + 'All_Collars'
     Full_path = out_FGDB + out_FC
     arcpy.env.outputMFlag = "Disabled"
@@ -30,7 +31,8 @@ try:
     arcpy.FeatureClassToFeatureClass_conversion(saved_Layer, out_FGDB, out_FC)
     arcpy.AddField_management(Full_path, 'Local_Date', 'DATE')
     arcpy.CalculateField_management(Full_path, 'Local_Date', '!Date___Time__Local_!', 'PYTHON')
-    arcpy.DeleteField_management(Full_path, 'Date___Time__GMT_', 'Date___Time__Local_')
+    arcpy.DeleteField_management(Full_path, 'Date___Time__GMT_')
+    arcpy.DeleteField_management(Full_path, 'Date___Time__Local_')
 
 except Exception as err:
     print(err.args[0])
@@ -44,12 +46,19 @@ with arcpy.da.SearchCursor('in_memory/device' ,['Device_ID']) as dissolve_featur
     for row in dissolve_feature:
         refNumber = str(row[0])
         feature = out_FGDB + '\\'
+        Heat_Feature = out_Heat + '\\'
         Selection_q = '"Device_ID" ='+ refNumber
-        print (Selection_q)
         print(refNumber)
+        def Heatprocess(Selection_Feature, Output_Feature, ):
+            arcpy.SelectLayerByAttribute_management('templayer', 'NEW_SELECTION',Selection_Feature)
+            arcpy.FeatureClassToFeatureClass_conversion('templayer', feature, Output_Feature)
+
         arcpy.SelectLayerByAttribute_management('templayer', 'NEW_SELECTION', Selection_q)
         arcpy.FeatureClassToFeatureClass_conversion('templayer', feature, 'Device_ID' + refNumber)
-
+        if refNumber == '44161':
+            #arcpy.SelectLayerByAttribute_management('templayer', 'NEW_SELECTION', "Device_ID = 44161 AND Date >= date '2017-04-25 00:00:00'")
+            #arcpy.FeatureClassToFeatureClass_conversion('templayer', Heat_Feature, 'Heatmap' + refNumber)
+            Heatprocess("Device_ID = 44161 AND Date >= date '2017-04-25 00:00:00'",'Heatmap' + refNumber)
 arcpy.Delete_management('templayer')
 arcpy.Delete_management('in_memory/device')
 del row
